ef576fcc8756431e1e5e6e6865dc2a46764f915b
samazolfkhanu <samazolfkhani0@gmail.com>
2025-06-11 06:44:06 -0700

    ThreadState

--- Thread1/Main.java
+++ Thread1/Main.java
@@ -4,9 +4,27 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Thread t=new Thread(new Run());
-        Thread t1=new Thread(new Run());
+        Thread t=new Thread(new ThreadState());
+        System.out.println(t.getState());
         t.start();
-        t1.start();
+        try
+        {
+            Thread.sleep(1000);
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(t.getState());
+        try
+        {
+            t.join();
+        }catch(InterruptedException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(t.getState());
+
     }
 }
+
